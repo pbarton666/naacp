012fb8050b1f91c150889c79400811b33ea40ba4
Pat Barton <barton.pj@gmail.com>
2015-11-08 14:13:49 -0800

    made build-all a bit more friendly

---
+++ test_misc.py
@@ -0,0 +1,24 @@
+
+"""miscellaneous tests"""
+import unittest
+import utils_and_settings
+
+class test_me(unittest.TestCase):
+    def test_names(self):
+        "makes sure column names appear as expected"
+        test='/home/pat/data/RL_Analysis/NoRed-Hwy/HWY_OD_TimeCost/HWY_OP_HOV_DISTANCE.csv'
+        table=utils_and_settings.get_table_name(test)
+        col = utils_and_settings.get_column_name('HWY_OP_HOV_DISTANCE.csv')
+        self.assertEqual(table, 'nored-hwy_hwy_od')
+        self.assertEqual(col, 'op_hov_distance')
+        x=1
+        
+    
+
+
+
+
+
+
+if __name__=='__main__':
+    unittest.main()
