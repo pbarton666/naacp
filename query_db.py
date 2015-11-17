f9b53d93c1169b01436a2073a443006b442dc094
pat <barton.pj@gmail.com>
2015-11-17 07:07:31 -0800

    added hwy distance

--- query_db.py
+++ query_db.py
@@ -121,32 +121,56 @@ def make_rollup_tables():
         scenario=prefix + scenario+'_'
 
 
-        #rail/bus costs = SUM((peak trips * peak fare) + (op trips * op fare))
+       
+        
         #Produces a bunch of SELECTS like this:
-        '''--adding peak rail/bus fare costs
-           SELECT  DISTINCT
-              test_yesred_nohwy_mode_choice_od.origin.
-              test_yesred_nohwy_mode_choice_od.dest,
-              test_yesred_nohwy_mode_choice_od.hbw_inc1_wexpbus * test_yesred_nohwy_mode_choice_od.wexpbus_pk
-          FROM 
-             test_yesred_nohwy_test_yesred_nohwy_mode_choice_od , test_fares_fares.wexpbus._pk  
-          WHERE  
-             test_yesred_nohwy_mode_choice_od.origin=test_fares_fares.origin AND 
-             test_yesred_nohwy_mode_choice_od.dest=test_fares_fares.dest 
-          ORDER BY 
-             test_yesred_nohwy_.origin, test_yesred_nohwy_mode_choice_od.dest'''
+
         
         
-        #create an empty np array for now
-        npa=None
+   
         
         #Gather data for each income group.  Handle off peak and peak separately (different fares
         #  and work-related purposes imply peak travel)
+     
+        
+            
+        
         for income, _ in incomes:
             
-            master_npa=None
+            master_npa=None #placeholder for array to be transferred to the db
             
-            #transit fare cost (tod, across purpose, mode pair) - 
+            '''This is pretty straighforward:  rail/bus costs = SUM((peak trips * peak fare) + (op trips * op fare))
+            
+                The main assumption is that work-related trips are on peak, and others are off.  Here are the mappings:
+                
+                purposes_pk=[('hbw', 1), ('nhbw', 2)]
+                purposes_op=[('obo', 3), ('hbs', 4), ('hbo', 5)]
+                
+                The mode_choice_od table disaggregates to income, mode, purpose.  For instance, we have data for
+                trips from home to work, walking to the express bus, on-peak for each OD pair.  From the fares_fares table,
+                we know the fare for the express bus on-peak.   
+                
+                So we can simply aggregate across all the mode choice/purpose pairs to capture all trips associated with some
+                income group and assign the correct fair to each.
+                
+                We do this by coming up with a bunch of SELECT statements like this one:
+                
+                '--adding peak rail/bus fare costs
+                   SELECT  DISTINCT
+                      test_yesred_nohwy_mode_choice_od.origin.
+                      test_yesred_nohwy_mode_choice_od.dest,
+                      test_yesred_nohwy_mode_choice_od.hbw_inc1_wexpbus * test_yesred_nohwy_mode_choice_od.wexpbus_pk
+                  FROM 
+                     test_yesred_nohwy_test_yesred_nohwy_mode_choice_od , test_fares_fares.wexpbus._pk  
+                  WHERE  
+                     test_yesred_nohwy_mode_choice_od.origin=test_fares_fares.origin AND 
+                     test_yesred_nohwy_mode_choice_od.dest=test_fares_fares.dest 
+                  ORDER BY 
+                     test_yesred_nohwy_.origin, test_yesred_nohwy_mode_choice_od.dest'''     
+            
+
+            #rail/bus costs = SUM((peak trips * peak fare) + (op trips * op fare))            
+            npa=None        #create an empty npa array placeholder 
             while True:
                 trips_table = '{}mode_choice_od'.format(scenario)
                 fares_table= '{}fares_fares'.format(prefix)
@@ -216,68 +240,83 @@ def make_rollup_tables():
                 logger.info('done rolling up transit costs for {} {}'.format(scenario, income))
                 break  #done with transit costs                
           
-            #transit fare cost (peak, across purpose, mode pair)
+            #highway distances (same logic for toll and drive time)
+            '''            
+            Distance = trips * distance / trip.  
+            
+            Trips come mode_choice_od.  Work-related trips are assumed on-peak.   
+            
+            Distances come from loaded_hwy_od_timecost.  Times are (am, pm, md, nt).  One question is how best to map the tod-based costs (toll, distance, drive time)
+            to our cobbled-together notion of on/off peak.
+            
+            Thinking that this assumption isn't highly-leveraged for the analysis - unless the tod of trips is driven by the Red Line or highway upgrades.
+            
+            Mappings:  For on-peak trips, we'll assume costs are an average of the am and pm costs.  For off-peak trips, we'll assume an average of md and nt costs.
+            
+                              Modes 'sr2' and 'sr3' are 'hov' occupancies;  mode 'da' is 'sov' occupancy
+                        
+            
+        
+            '''
+            #note that we're already in an income loop
+            npa=None
             while True:
+               
                 trips_table = '{}mode_choice_od'.format(scenario)
-                fares_table= '{}fares_fares'.format(prefix)
+                costs_table= '{}loaded_od_hwy_timecost'.format(scenario)
+                metric='time'
+                pk_flag='pk'
+                #peak
+                logger.info('peak driving distance costs')
                 for purpose, _ in purposes_pk:
-                    for mode, _ in bus_modes+rail_modes:
-                        #write a new SELECT statement for each purpose, mode pair
-                        select= '\n\n--adding peak rail/bus fare costs\n'
-                        select += 'SELECT  DISTINCT\n '
-                        #origin and dest
-                        select += '\t{}.origin.\n'.format(trips_table)
-                        select += '\t{}.dest,\n'.format(trips_table)                    
-                        stmt=       '\t{}.{}_{}_{} * {}.{}_pk\n '
-                        select+= stmt.format(trips_table, purpose, income, mode, trips_table, mode)
-                        select += 'FROM \n\t{}{} , {}.{}.{}  \n '.format(scenario, trips_table, fares_table, mode, '_pk')
-                        select +='WHERE  \n\t{}.origin={}.origin AND \n'.format( trips_table, fares_table)
-                        select +='\t{}.dest={}.dest \n'.format(trips_table, fares_table)
-                        select +='ORDER BY \n\t{}.origin, {}.dest\n\n'.format(scenario, trips_table,trips_table)                    
+                    for mode, _ in drive_modes:
+                        for occupancy, _  in occupancy_hwy_loaded:
+                            if mode=='da':
+                                loaded_occupancy='sov'
+                            else:
+                                loaded_occupancy='hov'
+                                
+                            #write a new SELECT statement for each purpose, mode pair
+                            select= '--adding {} driving distance cost\n'.format(pk_flag)
+                            select += 'SELECT  DISTINCT\n '
+                            
+                            #origin and dest
+                            select += '\t{}.origin,\n'.format(trips_table)
+                            select += '\t{}.dest,\n'.format(trips_table)    
+                            
+                            #here, we need to average the am and pm values
+                                #{trips_table}.{purpose}_{income}_{mode} * {cost_table}_hwy_{metric}_{loaded_occupancy}_{tod} /2 +  \n}
+                            stmt1=       '\t{}.{}_{}_{} * {}.hwy_{}_{}_am  /2 +\n '
+                            stmt2=       '\t{}.{}_{}_{} * {}.hwy_{}_{}_pm  /2  \n'
+                            c=1
+                            select+= stmt1.format(trips_table, purpose, income, mode,  costs_table, metric, loaded_occupancy)
+                            select+= stmt2.format(trips_table, purpose, income, mode,  costs_table, metric, loaded_occupancy)
+                            print(select)
+                            select += 'FROM \n\t{} , {} \n '.format( trips_table, costs_table)
+                            select +='WHERE  \n\t{}.origin={}.origin AND \n'.format( trips_table, costs_table)
+                            select +='\t{}.dest={}.dest \n'.format(trips_table, costs_table)
+                            select +='ORDER BY \n\t{}.origin, {}.dest\n\n'.format( trips_table,trips_table)                    
     
                         #grab the results. create a new np array or add this to the last (results) column                        
                         logger.info('writing data for peak transit fares for {}: {} {}{}'.format(scenario, income, purpose, mode))
                         logger.debug('executing {}\n'.format(select))
+                        print(select)
                         curs.execute(select)
                         res=np.array(curs.fetchall())
                         if npa is  None:
                             npa=res
                         else: 
                             npa[:,-1]+=res[:,-1]
-                #off peak, across purpose, mode pair           
-                for purpose, _ in purposes_op:
-                    for mode, _ in bus_modes+rail_modes:
-                        #write a new SELECT statement for each purpose, mode pair
-                        select= '\n\n--adding peak rail/bus fare costs\n'
-                        select += 'SELECT  DISTINCT\n '
-                        #origin and dest
-                        select += '\t{}.origin.\n'.format(trips_table)
-                        select += '\t{}.dest,\n'.format(trips_table)                    
-                        stmt=       '\t{}.{}_{}_{} * {}.{}_op\n '   #changed to reflect op
-                        select+= stmt.format(trips_table, purpose, income, mode, trips_table, mode)
-                        select += 'FROM \n\t{}{} , {}.{}.{}  \n '.format(scenario, trips_table, fares_table, mode, '_pk')
-                        select +='WHERE  \n\t{}.origin={}.origin AND \n'.format( trips_table, fares_table)
-                        select +='\t{}.dest={}.dest \n'.format(trips_table, fares_table)
-                        select +='ORDER BY \n\t{}.origin, {}.dest\n\n'.format(scenario, trips_table,trips_table)                    
-    
-                        #grab the results. create a new np array or add this to the last (results) column
-                        logger.info('writing data for off peak transit fares for {}: {} {}{}').format(scenario, income, purpose, mode)
-                        logger.debug('executing {}\n'.format(select))                        
-                        curs.execute(select)
-                        res=np.array(curs.fetchall())
-                        if npa is  None:
-                            npa=res
-                        else: 
-                            npa[:,-1]+=res[:,-1]      
-            
-                #Add this to master npa for transfer to the db.
-                if master_npa is  None:
-                    master_npa=npa
-                else: 
-                    master_npa[:,-1]+=npa[:,-1]          
-                    
-                logger.info('done rolling up transit costs for {} {}').format(scenario, income)
-                break  #done with transit costs    
+       
+                
+                    #Add this to master npa for transfer to the db.
+                    if master_npa is  None:
+                        master_npa=npa
+                    else: 
+                        master_npa[:,-1]+=npa[:,-1]          
+                        
+                    logger.info('done rolling up transit costs for {} {}').format(scenario, income)
+                    break  #done with transit costs    
 
 
 
